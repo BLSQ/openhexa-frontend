ea5e6b1a12155440b7270214e907043b205b17be
cheikhgwane <cwane@bluesquarehub.com>
2023-01-12 11:15:21 +0000

    check if user has a workspace on server-side

--- src/pages/workspaces/index.tsx
+++ src/pages/workspaces/index.tsx
@@ -1,34 +1,21 @@
 import Page from "core/components/Page";
 import { createGetServerSideProps } from "core/helpers/page";
-import { NextPageWithLayout } from "core/helpers/types";
 import useFeature from "identity/hooks/useFeature";
 import useMe from "identity/hooks/useMe";
 import { useRouter } from "next/router";
-import { ReactElement, useEffect, useState } from "react";
+import { ReactElement } from "react";
 import { useTranslation } from "react-i18next";
 import CreateWorkspaceDialog from "workspaces/features/CreateWorkspaceDialog";
-import { useWorkspacesPageQuery } from "workspaces/graphql/queries.generated";
+import {
+  WorkspacesPageDocument,
+  WorkspacesPageQuery,
+} from "workspaces/graphql/queries.generated";
 
-const WorkspacesHome: NextPageWithLayout = () => {
-  const { data, loading } = useWorkspacesPageQuery({});
-  const [openDialog, setOpenDialog] = useState(false);
+const WorkspacesHome = () => {
   const router = useRouter();
   const [hasWorkspacesEnabled] = useFeature("workspaces");
   const me = useMe();
   const { t } = useTranslation();
-
-  useEffect(() => {
-    if (!loading) {
-      if (data?.workspaces && data.workspaces.items.length) {
-        const latestWorkspace = data.workspaces.items[0];
-        router.push(`/workspaces/${latestWorkspace.id}`);
-      } else if (hasWorkspacesEnabled && me.permissions.createWorkspace) {
-        setOpenDialog(true);
-      } else {
-        router.push("/dashboard");
-      }
-    }
-  }, [data, router, loading, hasWorkspacesEnabled, me]);
 
   const handleClose = (workspaceId: string | null) => {
     if (!workspaceId) {
@@ -37,17 +24,40 @@ const WorkspacesHome: NextPageWithLayout = () => {
     router.push(`/workspaces/${workspaceId}`);
   };
 
-  return (
-    <Page title={t("New workspace")}>
-      <CreateWorkspaceDialog open={openDialog} onClose={handleClose} />
-    </Page>
-  );
+  if (hasWorkspacesEnabled && me.permissions.createWorkspace) {
+    return (
+      <Page title={t("New workspace")}>
+        <CreateWorkspaceDialog open onClose={handleClose} />
+      </Page>
+    );
+  } else {
+    router.push("/dashboard");
+  }
 };
 
 WorkspacesHome.getLayout = (page: ReactElement) => page;
 
 export const getServerSideProps = createGetServerSideProps({
   requireAuth: true,
-});
+  async getServerSideProps(ctx, client) {
+    const { data } = await client.query<WorkspacesPageQuery>({
+      query: WorkspacesPageDocument,
+      variables: {},
+    });
 
+    if (!data.workspaces || !data.workspaces.items.length) {
+      return {
+        props: {},
+      };
+    }
+
+    const latestWorkspace = data.workspaces.items[0];
+    return {
+      redirect: {
+        permanent: false,
+        destination: `/workspaces/${latestWorkspace.id}`,
+      },
+    };
+  },
+});
 export default WorkspacesHome;
