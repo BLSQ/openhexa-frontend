d0c4336c88c2c67a4aaefbde0c49cabb488e6a98
Cheikh Gueye Wane <cheikhgueye.wane@gmail.com>
2023-03-24 12:29:54 +0000

    Update src/workspaces/features/GenerateDatabasePasswordDialog/GenerateDatabasePasswordDialog.tsx
    Co-authored-by: Quentin Gérôme <geromequentin@gmail.com>

--- src/workspaces/features/GenerateDatabasePasswordDialog/GenerateDatabasePasswordDialog.tsx
+++ src/workspaces/features/GenerateDatabasePasswordDialog/GenerateDatabasePasswordDialog.tsx
@@ -56,7 +56,7 @@ const GenerateWorkspaceDatabasePasswordDialog = (
       <Dialog.Content className="space-y-4">
         <p>
           {t(
-            "You're about to generate a new password for the workspace database. This action cannot be undone and make sure to:"
+            "You're about to generate a new password for the workspace database. This action cannot be undone. Make sure to:"
           )}
         </p>
         <ul className="list list-inside list-disc">
