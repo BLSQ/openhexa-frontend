443802088171d4b16f076f3065f58e2fd4c4008f
Quentin Gérôme <quentin@letsbuildit.dev>
2023-04-12 09:43:20 +0200

    drawer

--- src/workspaces/layouts/WorkspaceLayout/Header.tsx
+++ src/workspaces/layouts/WorkspaceLayout/Header.tsx
@@ -1,5 +1,8 @@
+import { Bars3Icon, ChevronLeftIcon } from "@heroicons/react/24/outline";
 import clsx from "clsx";
-import { ReactNode } from "react";
+import Button from "core/components/Button";
+import { ReactNode, useContext } from "react";
+import { LayoutContext } from "./WorkspaceLayout";
 
 type HeaderProps = {
   children?: ReactNode;
@@ -8,14 +11,32 @@ type HeaderProps = {
 
 const Header = (props: HeaderProps) => {
   const { className, children } = props;
+  const { isOpen, setOpen } = useContext(LayoutContext);
   return (
     <div
       className={clsx(
         "sticky top-0 z-10 flex h-16 flex-shrink-0 items-center justify-between border-b border-gray-200 bg-white py-3 shadow",
-        "px-4 md:px-6 xl:px-10 2xl:px-12"
+        "group relative px-4 md:px-6 xl:px-10 2xl:px-12"
       )}
     >
-      <div className={clsx("flex-1", className)}>{children}</div>
+      <div
+        className={clsx(
+          "absolute left-2.5 flex cursor-pointer items-center transition-all",
+          isOpen && "opacity-0 duration-100 group-hover:opacity-100"
+        )}
+      >
+        <button
+          className="inline-flex h-fit items-center justify-center rounded focus:outline-none focus:ring-0"
+          onClick={() => setOpen(!isOpen)}
+        >
+          {isOpen ? (
+            <ChevronLeftIcon className="w-5" />
+          ) : (
+            <Bars3Icon className="w-5" />
+          )}
+        </button>
+      </div>
+      <div className={clsx("ml-6 flex-1", className)}>{children}</div>
     </div>
   );
 };
--- src/workspaces/layouts/WorkspaceLayout/Sidebar.tsx
+++ src/workspaces/layouts/WorkspaceLayout/Sidebar.tsx
@@ -75,65 +75,57 @@ const Sidebar = (props: SidebarProps) => {
   const { slug } = workspace;
 
   return (
-    <div className="fixed inset-y-0 flex w-64 flex-col">
-      <div className="flex flex-grow flex-col overflow-y-auto border-r border-gray-200 bg-gray-800">
-        <SidebarMenu workspace={workspace} />
+    <div className="flex flex-grow flex-col overflow-y-auto border-r border-gray-200 bg-gray-800">
+      <SidebarMenu workspace={workspace} />
 
-        <div className="mt-5 flex flex-grow flex-col">
-          <nav className="flex-1 space-y-1 px-0 pb-4">
-            <NavItem exact href={`/workspaces/${encodeURIComponent(slug)}`}>
-              <HomeIcon className="h-5 w-5" />
-              {t("Home")}
+      <div className="mt-5 flex flex-grow flex-col">
+        <nav className="flex-1 space-y-1 px-0 pb-4">
+          <NavItem exact href={`/workspaces/${encodeURIComponent(slug)}`}>
+            <HomeIcon className="h-5 w-5" />
+            {t("Home")}
+          </NavItem>
+          <NavItem href={`/workspaces/${encodeURIComponent(slug)}/files`}>
+            <FolderOpenIcon className="h-5 w-5" />
+            {t("Files")}
+          </NavItem>
+          <NavItem href={`/workspaces/${encodeURIComponent(slug)}/databases`}>
+            <CircleStackIcon className="h-5 w-5" />
+            {t("Database")}
+          </NavItem>
+          <NavItem href={`/workspaces/${encodeURIComponent(slug)}/connections`}>
+            <SwatchIcon className="h-5 w-5" />
+            {t("Connections")}
+          </NavItem>
+          <NavItem href={`/workspaces/${encodeURIComponent(slug)}/pipelines`}>
+            <ArrowPathIcon className="h-5 w-5" />
+            {t("Pipelines")}
+          </NavItem>
+          {workspace.permissions.update && (
+            <NavItem href={`/workspaces/${encodeURIComponent(slug)}/notebooks`}>
+              <BookOpenIcon className="h-5 w-5" />
+              {t("JupyterHub")}
             </NavItem>
-            <NavItem href={`/workspaces/${encodeURIComponent(slug)}/files`}>
-              <FolderOpenIcon className="h-5 w-5" />
-              {t("Files")}
+          )}
+          {workspace.permissions.manageMembers && (
+            <NavItem href={`/workspaces/${encodeURIComponent(slug)}/settings`}>
+              <Cog6ToothIcon className="h-5 w-5" />
+              {t("Settings")}
             </NavItem>
-            <NavItem href={`/workspaces/${encodeURIComponent(slug)}/databases`}>
-              <CircleStackIcon className="h-5 w-5" />
-              {t("Database")}
-            </NavItem>
-            <NavItem
-              href={`/workspaces/${encodeURIComponent(slug)}/connections`}
-            >
-              <SwatchIcon className="h-5 w-5" />
-              {t("Connections")}
-            </NavItem>
-            <NavItem href={`/workspaces/${encodeURIComponent(slug)}/pipelines`}>
-              <ArrowPathIcon className="h-5 w-5" />
-              {t("Pipelines")}
-            </NavItem>
-            {workspace.permissions.update && (
-              <NavItem
-                href={`/workspaces/${encodeURIComponent(slug)}/notebooks`}
-              >
-                <BookOpenIcon className="h-5 w-5" />
-                {t("JupyterHub")}
-              </NavItem>
-            )}
-            {workspace.permissions.manageMembers && (
-              <NavItem
-                href={`/workspaces/${encodeURIComponent(slug)}/settings`}
-              >
-                <Cog6ToothIcon className="h-5 w-5" />
-                {t("Settings")}
-              </NavItem>
-            )}
-          </nav>
-        </div>
-        <div className="mb-5 flex flex-shrink-0 flex-col items-center px-4">
-          <Link
-            noStyle
-            href="/"
-            className="relative flex h-8 flex-shrink-0 items-center"
-          >
-            <img
-              className="h-full"
-              src="/images/logo_with_text_white.svg"
-              alt="OpenHexa logo"
-            />
-          </Link>
-        </div>
+          )}
+        </nav>
+      </div>
+      <div className="mb-5 flex flex-shrink-0 flex-col items-center px-4">
+        <Link
+          noStyle
+          href="/"
+          className="relative flex h-8 flex-shrink-0 items-center"
+        >
+          <img
+            className="h-full"
+            src="/images/logo_with_text_white.svg"
+            alt="OpenHexa logo"
+          />
+        </Link>
       </div>
     </div>
   );
