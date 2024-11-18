7ab664d701086e436b6f00d02aedd0760b41fa46
Quentin Gérôme <geromequentin@gmail.com>
2024-11-18 12:12:37 +0100

    fix(Apollo): Set initialFetchPolicy to 'cache-first' and implement a basic nextFetchPolicy
    The nextFetchPolicy is taken from the Apollo documentation

--- src/core/helpers/apollo.ts
+++ src/core/helpers/apollo.ts
@@ -137,7 +137,42 @@ const createApolloClient = (headers: IncomingHttpHeaders | null = null) => {
     cache,
     defaultOptions: {
       watchQuery: {
-        fetchPolicy: "cache-and-network",
+        initialFetchPolicy: "cache-first",
+        nextFetchPolicy(
+          currentFetchPolicy,
+          {
+            // Either "after-fetch" or "variables-changed", indicating why the
+            // nextFetchPolicy function was invoked.
+            reason,
+            // The rest of the options (currentFetchPolicy === options.fetchPolicy).
+            options,
+            // The original value of options.fetchPolicy, before nextFetchPolicy was
+            // applied for the first time.
+            initialFetchPolicy,
+            // The ObservableQuery associated with this client.watchQuery call.
+            observable,
+          },
+        ) {
+          // When variables change, the default behavior is to reset
+          // options.fetchPolicy to context.initialFetchPolicy. If you omit this logic,
+          // your nextFetchPolicy function can override this default behavior to
+          // prevent options.fetchPolicy from changing in this case.
+          if (reason === "variables-changed") {
+            return initialFetchPolicy;
+          }
+
+          if (
+            currentFetchPolicy === "network-only" ||
+            currentFetchPolicy === "cache-and-network"
+          ) {
+            // Demote the network policies (except "no-cache") to "cache-first"
+            // after the first request.
+            return "cache-first";
+          }
+
+          // Leave all other fetch policies unchanged.
+          return currentFetchPolicy;
+        },
       },
     },
   });
