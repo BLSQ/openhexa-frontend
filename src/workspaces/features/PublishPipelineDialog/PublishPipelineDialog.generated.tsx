1ff624b0817fe319320d801e6296c702bc1aee8e
YolanFery <yolan@codethefuture.be>
2024-12-17 11:08:25 +0100

    Form fix

---
+++ src/workspaces/features/PublishPipelineDialog/PublishPipelineDialog.generated.tsx
@@ -0,0 +1,24 @@
+import * as Types from '../../../graphql/types';
+
+import { gql } from '@apollo/client';
+export type PipelinePublish_PipelineFragment = { __typename?: 'Pipeline', id: string, currentVersion?: { __typename?: 'PipelineVersion', id: string, versionName: string } | null, template?: { __typename?: 'PipelineTemplate', name: string } | null };
+
+export type PipelinePublish_WorkspaceFragment = { __typename?: 'Workspace', slug: string };
+
+export const PipelinePublish_PipelineFragmentDoc = gql`
+    fragment PipelinePublish_pipeline on Pipeline {
+  id
+  currentVersion {
+    id
+    versionName
+  }
+  template {
+    name
+  }
+}
+    `;
+export const PipelinePublish_WorkspaceFragmentDoc = gql`
+    fragment PipelinePublish_workspace on Workspace {
+  slug
+}
+    `;
