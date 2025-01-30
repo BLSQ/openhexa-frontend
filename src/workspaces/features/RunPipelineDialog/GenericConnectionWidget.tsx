21cf4dfe82f50000c81ca13a8a0060c21509dd34
nazarfil <nfilipchuk@bluesquarehub.com>
2025-01-30 16:21:32 +0100

    add combbox

--- src/workspaces/features/RunPipelineDialog/GenericConnectionWidget.tsx
+++ src/workspaces/features/RunPipelineDialog/GenericConnectionWidget.tsx
@@ -1,19 +1,16 @@
 import { gql, useQuery } from "@apollo/client";
-import React, { useCallback, useMemo } from "react";
-import Select from "core/components/forms/Select";
-import { PipelineNotificationLevel } from "../../../graphql/types";
-import { formatNotificationLevel } from "../../helpers/recipients/utils";
+import React, { useCallback, useMemo, useState } from "react";
 import { i18n } from "next-i18next";
-import { ensureArray } from "../../../core/helpers/array";
 import {
   Combobox,
   MultiCombobox,
 } from "../../../core/components/forms/Combobox";
+import useDebounce from "../../../core/hooks/useDebounce";
 
-type GenericConnectionWidgetProps = {
+type GenericConnectionWidgetProps<T> = {
   parameter: any;
-  value: string | null;
-  onChange: (value: string | null) => void;
+  value: T[] | T;
+  onChange: (value: any) => void;
   workspaceSlug: string;
 };
 
@@ -51,29 +48,16 @@ const widgetToQueryType: { [key: string]: string } = {
   indicator_groups_picker: "INDICATOR_GROUPS",
 };
 
-const GenericConnectionWidget = ({
+const GenericConnectionWidget = <T,>({
   parameter,
   form,
   value,
   onChange,
   workspaceSlug,
-}: GenericConnectionWidgetProps) => {
-  console.log("Parameter", parameter);
+}: GenericConnectionWidgetProps<T>) => {
+  console.log("Value", value);
   console.log("Form", form);
   console.log("Widget", widgetToQueryType[parameter.widget]);
-
-  const handleChange = useCallback(
-    (value: any) => {
-      if (parameter.multiple && (value === null || value === undefined)) {
-        return onChange([]);
-      } else if (parameter.multiple && !parameter.choices) {
-        onChange(value.split(","));
-      } else {
-        onChange(value);
-      }
-    },
-    [onChange, parameter.multiple, parameter.choices],
-  );
 
   const { data, loading, error } = useQuery(GET_CONNECTION_METADATA, {
     variables: {
@@ -85,39 +69,46 @@ const GenericConnectionWidget = ({
   });
 
   // Convert API response into Select-compatible options
+  const [query, setQuery] = useState("");
+  const debouncedQuery = useDebounce(query, 150);
   const options = useMemo(() => {
     if (error) {
       console.error("Error fetching connection metadata:", error);
       return [];
     }
+    const lowercaseQuery = debouncedQuery.toLowerCase();
     return (
-      data?.connectionBySlug?.queryMetadata?.items?.map(
-        (item: { id: string; name: string }) => ({
-          value: item.id,
-          label: item.name,
-        }),
-      ) || []
+      data?.connectionBySlug?.queryMetadata?.items?.filter((c) =>
+        c.name.toLowerCase().includes(lowercaseQuery),
+      ) ?? []
     );
   }, [data, error]);
-  console.log("Options", options);
+
   const PickerComponent: any = parameter.multiple ? MultiCombobox : Combobox;
+
   return (
-    <Select
-      value={parameter.multiple ? ensureArray(value) : value}
+    <PickerComponent
+      required={parameter.required}
+      onChange={onChange}
       loading={loading}
-      displayValue={(value) => value?.label}
+      displayValue={(value) => value?.map((v: any) => v.name).join("")}
+      by="id"
+      onInputChange={useCallback(
+        (event: any) => setQuery(event.target.value),
+        [],
+      )}
       placeholder={i18n!.t("Select notification level")}
-      onChange={handleChange}
-      required={Boolean(parameter.required)}
-      getOptionLabel={(option) => option.label}
-      options={options}
-      filterOptions={(options, query) => {
-        console.log("Query", query);
-        return options.filter((opt) =>
-          opt.label.toLowerCase().includes(query.toLowerCase()),
-        );
-      }}
-    />
+      value={value as any}
+      onClose={useCallback(() => setQuery(""), [])}
+      disabled={loading}
+      withPortal={false}
+    >
+      {options.map((option) => (
+        <Combobox.CheckOption key={option.id} value={option}>
+          <div className="flex items-center">{option.name}</div>
+        </Combobox.CheckOption>
+      ))}
+    </PickerComponent>
   );
 };
 
