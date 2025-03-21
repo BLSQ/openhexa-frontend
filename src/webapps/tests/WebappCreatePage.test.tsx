75119fcf9f0c22c5a9da17dbf1ec221de5f6f1fa
YolanFery <yolan@codethefuture.be>
2025-03-21 09:54:52 +0100

    refactor test file 2

--- src/webapps/tests/WebappCreatePage.test.tsx
+++ src/webapps/tests/WebappCreatePage.test.tsx
@@ -7,9 +7,11 @@ import {
   within,
 } from "@testing-library/react";
 import WebappCreatePage from "pages/workspaces/[workspaceSlug]/webapps/create";
-import { useMutation, useQuery } from "@apollo/client";
 import { toast } from "react-toastify";
-import { useCreateWebappMutation } from "webapps/graphql/mutations.generated";
+import { TestApp } from "core/helpers/testutils";
+import { SidebarMenuDocument } from "workspaces/features/SidebarMenu/SidebarMenu.generated";
+import { CreateWebappDocument } from "webapps/graphql/mutations.generated";
+import { MockedResponse } from "@apollo/client/testing";
 
 jest.mock("react-toastify", () => ({
   toast: {
@@ -22,17 +24,6 @@ jest.mock("next-i18next", () => ({
   useTranslation: jest.fn().mockReturnValue({ t: (key: string) => key }),
 }));
 
-const mockMe = jest.fn(() => ({
-  features: [{ code: "webapps", config: {} }],
-  avatar: {
-    initials: "TU",
-    color: "",
-  },
-  permissions: {
-    createWorkspace: false,
-  },
-}));
-
 const mockWorkspace = {
   slug: "test-workspace",
   countries: [],
@@ -42,70 +33,67 @@ const mockWorkspace = {
   },
 };
 
-jest.mock("identity/hooks/useMe", () => () => mockMe());
-
-jest.mock("@apollo/client", () => ({
-  __esModule: true,
-  useQuery: jest.fn(),
-  useMutation: jest.fn(),
-  gql: jest.fn(() => "GQL"),
-}));
-
-const useQueryMock = useQuery as jest.Mock;
-const useMutationMock = useMutation as jest.Mock;
-useMutationMock.mockReturnValue([jest.fn(), { loading: false }]);
-const sideBarMocks = {
-  pendingWorkspaceInvitations: { totalItems: 1 },
-  workspaces: {
-    totalItems: 2,
-    items: [
-      {
-        slug: "workspace-1",
-        name: "Workspace 1",
-        countries: [{ code: "US", flag: "🇺🇸" }],
+const graphqlMocks: MockedResponse[] = [
+  {
+    request: {
+      query: SidebarMenuDocument,
+      variables: {
+        page: 1,
+        perPage: 5,
       },
-      {
-        slug: "workspace-2",
-        name: "Workspace 2",
-        countries: [{ code: "FR", flag: "🇫🇷" }],
-      },
-    ],
-  },
-};
-
-useQueryMock.mockReturnValue({
-  data: {
-    workspace: mockWorkspace,
-    ...sideBarMocks,
-  },
-  loading: false,
-  error: null,
-});
-
-jest.mock("webapps/graphql/mutations.generated", () => ({
-  useCreateWebappMutation: jest.fn(),
-  useUpdateWebappMutation: () => [jest.fn()],
-}));
-
-const mockCreateWebapp = jest.fn();
-(useCreateWebappMutation as jest.Mock).mockReturnValue([
-  mockCreateWebapp,
-  { loading: false },
-]);
-mockCreateWebapp.mockResolvedValue({
-  data: {
-    createWebapp: {
-      webapp: {
-        id: "1",
-        name: "Test Webapp",
+    },
+    result: {
+      data: {
+        pendingWorkspaceInvitations: { totalItems: 1 },
+        workspaces: {
+          totalItems: 2,
+          items: [
+            {
+              slug: "workspace-1",
+              name: "Workspace 1",
+              countries: [{ code: "US", flag: "🇺🇸" }],
+            },
+            {
+              slug: "workspace-2",
+              name: "Workspace 2",
+              countries: [{ code: "FR", flag: "🇫🇷" }],
+            },
+          ],
+        },
       },
     },
   },
-});
+  {
+    request: {
+      query: CreateWebappDocument,
+      variables: {
+        input: {
+          workspaceSlug: "test-workspace",
+          name: "Test Webapp",
+          url: "http://test-webapp.com",
+        },
+      },
+    },
+    result: {
+      data: {
+        createWebapp: {
+          webapp: {
+            id: "1",
+            name: "Test Webapp",
+          },
+        },
+      },
+    },
+  },
+];
 
 describe("WebappCreatePage", () => {
   it("can create a web app", async () => {
-    render(<WebappCreatePage workspace={mockWorkspace} />);
+    render(
+      <TestApp mocks={graphqlMocks} me={{ features: [{ code: "webapps" }] }}>
+        <WebappCreatePage workspace={mockWorkspace} />
+      </TestApp>,
+    );
 
     await waitFor(() => {
       expect(screen.getByText("New Webapp")).toBeInTheDocument();
