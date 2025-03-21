1d687b5a2de39fecf0cc521168189b25366e457f
YolanFery <yolan@codethefuture.be>
2025-03-21 10:23:49 +0100

    refactor test file 3 incomplete

--- src/webapps/tests/WebappsPage.test.tsx
+++ src/webapps/tests/WebappsPage.test.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 import { fireEvent, render, screen, waitFor } from "@testing-library/react";
-import { useMutation, useQuery } from "@apollo/client";
 import WebappsPage from "pages/workspaces/[workspaceSlug]/webapps";
 import { toast } from "react-toastify";
+import { TestApp } from "core/helpers/testutils";
+import { SidebarMenuDocument } from "workspaces/features/SidebarMenu/SidebarMenu.generated";
+import { MockedResponse } from "@apollo/client/testing";
+import { WorkspaceWebappsPageDocument } from "workspaces/graphql/queries.generated";
 
 jest.mock("react-toastify", () => ({
   toast: {
@@ -10,93 +13,111 @@ jest.mock("react-toastify", () => ({
     error: jest.fn(),
   },
 }));
-const mockMe = jest.fn(() => ({
-  features: [{ code: "webapps", config: {} }],
-  avatar: {
-    initials: "TU",
-    color: "",
-  },
-  permissions: {
-    createWorkspace: false,
-  },
-}));
 
-jest.mock("identity/hooks/useMe", () => () => mockMe());
-jest.mock("identity/hooks/useFeature", () => jest.fn().mockReturnValue([true]));
-jest.mock("@apollo/client", () => ({
-  __esModule: true,
-  useQuery: jest.fn(),
-  useMutation: jest.fn(),
-  gql: jest.fn(() => "GQL"),
+jest.mock("next-i18next", () => ({
+  useTranslation: jest.fn().mockReturnValue({ t: (key: string) => key }),
 }));
-
-const useQueryMock = useQuery as jest.Mock;
-const useMutationMock = useMutation as jest.Mock;
-useMutationMock.mockReturnValue([jest.fn(), { loading: false }]);
 
 const mockWorkspace = {
+  __typename: "Workspace",
   slug: "test-workspace",
+  name: "Test Workspace",
   countries: [],
   permissions: {
     launchNotebookServer: false,
     manageMembers: false,
+    update: true,
   },
 };
 
 const webapp = (id: string) => ({
+  __typename: "Webapp",
   id: id,
   name: `Webapp ${id}`,
   isFavorite: false,
+  description: "Webapp description",
+  url: `https://example${id}.com`,
   icon: "",
   createdBy: {
+    __typename: "User",
+    id: id,
     displayName: `User ${id}`,
+    firstName: `FirstName ${id}`,
+    lastName: `LastName ${id}`,
+    email: `email${id}@email.com`,
     avatar: {
       initials: "U",
       color: "",
     },
   },
-});
-const mockWebapps = {
-  pageNumber: 1,
-  totalPages: 2,
-  totalItems: 16,
-  items: Array.from({ length: 15 }, (_, index) =>
-    webapp((index + 1).toString()),
-  ),
-};
-
-const sideBarMocks = {
-  pendingWorkspaceInvitations: { totalItems: 1 },
-  workspaces: {
-    totalItems: 2,
-    items: [
-      {
-        slug: "workspace-1",
-        name: "Workspace 1",
-        countries: [{ code: "US", flag: "🇺🇸" }],
-      },
-      {
-        slug: "workspace-2",
-        name: "Workspace 2",
-        countries: [{ code: "FR", flag: "🇫🇷" }],
-      },
-    ],
+  permissions: {
+    update: true,
+    delete: true,
   },
-};
+  workspace: mockWorkspace,
+});
+
+const graphqlMocks: MockedResponse[] = [
+  {
+    request: {
+      query: SidebarMenuDocument,
+      variables: {
+        page: 1,
+        perPage: 5,
+      },
+    },
+    result: {
+      data: {
+        pendingWorkspaceInvitations: { totalItems: 1 },
+        workspaces: {
+          totalItems: 2,
+          items: [
+            {
+              slug: "workspace-1",
+              name: "Workspace 1",
+              countries: [{ code: "US", flag: "🇺🇸" }],
+            },
+            {
+              slug: "workspace-2",
+              name: "Workspace 2",
+              countries: [{ code: "FR", flag: "🇫🇷" }],
+            },
+          ],
+        },
+      },
+    },
+  },
+  {
+    request: {
+      query: WorkspaceWebappsPageDocument,
+      variables: {
+        page: 1,
+        perPage: 15,
+      },
+    },
+    result: {
+      data: {
+        workspace: mockWorkspace,
+        webapps: {
+          pageNumber: 1,
+          totalPages: 2,
+          totalItems: 16,
+          items: Array.from({ length: 15 }, (_, index) =>
+            webapp((index + 1).toString()),
+          ),
+        },
+      },
+    },
+  },
+];
 
 describe("WebappsPage", () => {
   it("renders the list of webapps", async () => {
-    useQueryMock.mockReturnValue({
-      loading: false,
-      data: {
-        workspace: mockWorkspace,
-        webapps: mockWebapps,
-        ...sideBarMocks,
-      },
-      error: null,
-    });
-
-    render(<WebappsPage page={1} perPage={15} />);
+    render(
+      <TestApp mocks={graphqlMocks} me={{ features: [{ code: "webapps" }] }}>
+        <WebappsPage page={1} perPage={15} />
+      </TestApp>,
+    );
 
     await waitFor(() => {
       expect(screen.getByText("Webapp 1")).toBeInTheDocument();
@@ -104,17 +125,11 @@ describe("WebappsPage", () => {
   });
 
   it("adds a webapp to favorites", async () => {
-    useQueryMock.mockReturnValue({
-      loading: false,
-      data: {
-        workspace: mockWorkspace,
-        webapps: mockWebapps,
-        ...sideBarMocks,
-      },
-      error: null,
-    });
-
-    render(<WebappsPage page={1} perPage={15} />);
+    render(
+      <TestApp mocks={graphqlMocks} me={{ features: [{ code: "webapps" }] }}>
+        <WebappsPage page={1} perPage={15} />
+      </TestApp>,
+    );
 
     await waitFor(() => {
       expect(screen.getByText("Webapp 1")).toBeInTheDocument();
@@ -128,24 +143,11 @@ describe("WebappsPage", () => {
   });
 
   it("removes a webapp from favorites", async () => {
-    const updatedMocks = {
-      ...mockWebapps,
-      items: mockWebapps.items.map((item) =>
-        item.id === "1" ? { ...item, isFavorite: true } : item,
-      ),
-    };
-
-    useQueryMock.mockReturnValue({
-      loading: false,
-      data: {
-        workspace: mockWorkspace,
-        webapps: updatedMocks,
-        ...sideBarMocks,
-      },
-      error: null,
-    });
-
-    render(<WebappsPage page={1} perPage={15} />);
+    render(
+      <TestApp mocks={graphqlMocks} me={{ features: [{ code: "webapps" }] }}>
+        <WebappsPage page={1} perPage={15} />
+      </TestApp>,
+    );
 
     await waitFor(() => {
       expect(screen.getByText("Webapp 1")).toBeInTheDocument();
@@ -159,34 +161,17 @@ describe("WebappsPage", () => {
   });
 
   it("handles pagination", async () => {
-    useQueryMock.mockReturnValue({
-      loading: false,
-      data: {
-        workspace: mockWorkspace,
-        webapps: mockWebapps,
-        ...sideBarMocks,
-      },
-      error: null,
-    });
+    render(
+      <TestApp mocks={graphqlMocks} me={{ features: [{ code: "webapps" }] }}>
+        <WebappsPage page={1} perPage={15} />
+      </TestApp>,
+    );
 
-    render(<WebappsPage page={1} perPage={15} />);
     const previousButton = screen.getByRole("button", { name: /Previous/i });
     const nextButton = previousButton.nextElementSibling as HTMLButtonElement;
+
     await waitFor(() => {
       expect(nextButton).toBeInTheDocument();
-    });
-
-    useQueryMock.mockReturnValue({
-      loading: false,
-      data: {
-        workspace: mockWorkspace,
-        webapps: {
-          ...mockWebapps,
-          items: [webapp("16")],
-        },
-        ...sideBarMocks,
-      },
-      error: null,
     });
 
     fireEvent.click(nextButton);
